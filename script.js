732fed90feb5044ad0f4341ffac41762f19c64b9
Joshua272008 <113116525+Joshua272008@users.noreply.github.com>
2024-05-17 12:54:08 -0400

    as

--- script.js
+++ script.js
@@ -1,25 +1,81 @@
-function select(name) {
-    let item = {
-        name: name
-    };
-    var cartStuff = JSON.parse(localStorage.getItem('cart')) || [];
+var player;
+var comp;
 
-    cartStuff.push(item)
-
-    localStorage.setItem('cart', JSON.stringify(cartStuff));
-}
-
-function selectRock()
+function rock()
 {
-    select("Rock");
+    player = "rock";
+}
+function paper()
+{
+    player = "paper";
+
+    console.log("player chose paper");
+}
+function scissor()
+{
+    player = "scissor";
 }
 
-function selectPaper()
+function compChoice()
 {
-    select("Paper")
+    var rpc = ["rock", "paper", "scissors"]
+    comp = Math.floor(Math.random() * 3);
+    return rpc[comp];
 }
 
-function selectScisor()
-{
-    select("Paper")
+function shoot() {
+    var compWin = "Computer Wins"
+    var playerWin = "Player Wins"
+    let winner;
+
+
+    compChoice();
+
+    if(player === comp)
+    {
+        console.log("Computer Wins");
+        winner = "It's a tie!"
+        document.getElementById("winnerDisplayed").innerHTML = winner;
+    }
+    else if(player === "rock" && comp === "paper")
+    {
+            console.log("Computer Wins")
+        winner = "Computer Wins"
+        document.getElementById("winnerDisplayed").innerHTML = winner;
+    }
+    else if(player === "rock" && comp === "scissor")
+    {
+        console.log("Player Wins")
+        winner = "Player Wins"
+        document.getElementById("winnerDisplayed").innerHTML = winner;
+    }
+    else if(player === "paper" && comp === "scissor")
+    {
+        console.log("Computer Wins")
+        winner = "Computer Wins"
+        document.getElementById("winnerDisplayed").innerHTML = winner;
+    }
+    else if(player === "paper" && comp === "rock")
+    {
+        console.log("Player Wins")
+        winner = "Player Wins"
+        document.getElementById("winnerDisplayed").innerHTML = winner;
+    }
+    else if(player === "scissor" && comp === "rock")
+    {
+        console.log("Computer Wins")
+        winner = "Computer Wins"
+
+        document.getElementById("winnerDisplayed").innerHTML = winner;
+    }
+    else if(player === "scissor" && comp === "paper")
+    {
+        console.log("Player Wins")
+        winner = "Player Wins"
+        document.getElementById("winnerDisplayed").innerHTML += winner;
+
+    }
+    document.getElementById("winnerDisplayed").innerHTML += winner;
+
 }
+
